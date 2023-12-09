ef5284982486968d670578662a35ef23b0b86a7c
Vladimir <110772568+Vova4o@users.noreply.github.com>
2023-12-09 14:05:12 +0300

    adding array

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type Room struct {
+	name    string
+	cleaned bool
+}
+
+func checkCleanliness(prop [4]Room) {
+	for i := 0; i < len(prop); i++ {
+		room := prop[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is durty")
+		}
+	}
+}
+
 func main() {
 
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouce"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Performing some cleaning")
+
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
 }
